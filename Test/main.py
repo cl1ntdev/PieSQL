8b083abec5c25b478453a4954e6f8f0ac6f7ac6a
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-07-31 16:21:30 +0800

    testing Folder

---
+++ Test/main.py
@@ -0,0 +1,18 @@
+from copy import Error
+from connector.DbConnection import createConnection
+
+host = '127.0.0.1'
+username = 'root'
+password = 'clinT'
+db = "pyweirdool"
+port =3306
+
+def main():
+    try:
+        connection = createConnection(host,username,password,db)
+        if connection:
+            print("suceessfully connected")
+        else: 
+            print("error")
+    except Error as e:
+        print(e)
