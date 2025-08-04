8c91e904ad77a38fbc49d2f7d2235cafc87b7c09
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-08-04 22:08:45 +0800

    added main/defualt host

--- connector/GlobalHost.py
+++ connector/GlobalHost.py
@@ -1,6 +1,17 @@
+
+  
 class Host:
     def __init__(self,host,username,password,db):
         self.host = host
         self.username = username
         self.password = password
         self.db = db
+        
+        
+host = '127.0.0.1'
+username = 'root'
+password = 'clinT'
+db = "pyweirdool"
+port =3306
+
+MainHost = Host(host,username,password,db)
