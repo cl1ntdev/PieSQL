c9f8647dd6b8cf0281fb23bf83cf9e99f25dda13
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-08-05 21:24:37 +0800

    init constraint req

--- GET/Trigger/GetDataTrigger.py
+++ GET/Trigger/GetDataTrigger.py
@@ -13,4 +13,22 @@ def GetAllData(conn,tableName):
         return {"data": result}
     except Error as e:
         print("Error is ",e)
+        
+def GetAllDataConst(conn,tableName,constraint):
+    # constraint must start from "where"
+    try:
+        query = "Select * from " + tableName + " " + constraint
+        connection = None
+        try:
+            connection = conn
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute(query)
+            result = cursor.fetchall()
+            conn.close()
+            return {"data": result}
+        except Error as e:
+            print("Error is ",e)
+    except Error as e:
+        print("error is + ",e)
+    
     
