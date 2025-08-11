20d64f322b39582ce656240f11fd50d6d0aba6bd
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-08-11 22:14:44 +0800

    change class structure

--- GET/Class/InsertClass.py
+++ GET/Class/InsertClass.py
@@ -1,8 +1,13 @@
 class InsertDataTable():
-    def __init__(self,tableName,arrValues):
-        self.tableName = tableName
-        self.arrValues = arrValues #should be an array
-    
-    def insertData(self):   
+    def __init__(self):
+        self.tableName = ""
         
+    def defineSelf(self,tableName,arrValues):
+        self.tableName = tableName
+        self.arrValues = arrValues
+    
+    def addValues(self,arrValues):
+        self.arrValues = arrValues
+        
+    def insertData(self):   
         return "the total number of array is ", len(self.arrValues) 
