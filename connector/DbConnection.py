0493e89df010978dcd4090df7839ba14e71a0402
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-08-16 21:04:08 +0800

    global host alternative

--- connector/DbConnection.py
+++ connector/DbConnection.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from mysql.connector import Connect, Error
 from fastapi import FastAPI
+from connector.GlobalHost import MainHost
 
 app = FastAPI()
 
@@ -19,7 +20,7 @@ def createConnection(host,username,password,db):
         print("Error found: ",e)
     return connection
 
-
+# This is the global host alternative, define variables here
 def defaultConnection():
     host = '127.0.0.1'
     username = 'root'
