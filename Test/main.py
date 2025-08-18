83f55e947b6ccc343fcdf8e76a92bea1dca905ed
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-08-18 21:04:04 +0800

    program loop

--- Test/main.py
+++ Test/main.py
@@ -10,6 +10,7 @@ db = "pyweirdool"
 port =3306
 
 def main():
+   isEnd = False
    title = """
     ██████╗░██╗███████╗░██████╗░██████╗░██╗░░░░░
     ██╔══██╗██║██╔════╝██╔════╝██╔═══██╗██║░░░░░
@@ -20,61 +21,64 @@ def main():
     by - clint
     """
    print(title)
-    
+   
+   
+   while not isEnd:
 # Crud
-   print(">> Read DATA <<")
-   print("1. Specific Data") # done
-   print("2. All Data") # done
-
-   print(">> INSERT DATA << ")
-   print("3. Specific Data")
-
-   print(">> UPDATE DATA << ")
-   print("5. Specific Data")
-
-   print(">> DELETE DATA << ")
-   print("6. Specific Data")
-
-
-   ch = int(input("Choice: "))
-
-   match ch:
-        case 1:
-            dbConnection = defaultConnection()
-            tableName = "word"
-            colConst = "id"
-            colConstVal = "1-str"
-            data = GetClass()
-            data.defineConnection(defaultConnection)
-            data.checkConnection()
-            data.readAllDataConst(tableName,colConst,colConstVal)
-        case 2:
-            print("Choice is "+ str(ch) +" result is: ")
-            tableName = "word"
-            data = GetClass()
-            data.readData(tableName)
-            print(data)
-        case 3:
-            tableName = "food"
-            valuesToInsert = ["macaron","1-str","nice"] # pass in the values here any valus
-            columnNames = ["column1","column2","column3"] # pass the column to with they are assigned
-            insert = GetClass()
-           
-            insert.insertData(tableName,columnNames,valuesToInsert)
-        case 5:
-            table = "food"
-            col1 = "sweet"
-            col1Val = "candy-str"
-            colConstraint = "ID"
-            colConstraintVal = "1"
-            update = GetClass()
-            update.updateData(table,col1,col1Val,colConstraint,colConstraintVal)
-        case 6:
-            table = "word"
-            colConstraint = "id"
-            colConstraintVal = "1"
-            delete = GetClass()
-            delete.deleteData(table,colConstraint,colConstraintVal)
+    print(">> Read DATA <<")
+    print("1. Specific Data") # done
+    print("2. All Data") # done
+    
+    print(">> INSERT DATA << ")
+    print("3. Specific Data")
+    
+    print(">> UPDATE DATA << ")
+    print("5. Specific Data")
+    
+    print(">> DELETE DATA << ")
+    print("6. Specific Data")
+    
+    
+    ch = int(input("Choice: "))
+    
+    match ch:
+            case 1:
+                dbConnection = defaultConnection()
+                tableName = "word"
+                colConst = "id"
+                colConstVal = "1-str"
+                data = GetClass()
+                data.defineConnection(defaultConnection)
+                data.checkConnection()
+                data.readAllDataConst(tableName,colConst,colConstVal)
+            case 2:
+                print("Choice is "+ str(ch) +" result is: ")
+                tableName = "word"
+                data = GetClass()
+                data.readData(tableName)
+                print(data)
+            case 3:
+                tableName = "food"
+                valuesToInsert = ["macaron","1-str","nice"] # pass in the values here any valus
+                columnNames = ["column1","column2","column3"] # pass the column to with they are assigned
+                insert = GetClass()
+            
+                insert.insertData(tableName,columnNames,valuesToInsert)
+            case 5:
+                table = "food"
+                col1 = "sweet"
+                col1Val = "candy-str"
+                colConstraint = "ID"
+                colConstraintVal = "1"
+                update = GetClass()
+                update.updateData(table,col1,col1Val,colConstraint,colConstraintVal)
+            case 6:
+                table = "word"
+                colConstraint = "id"
+                colConstraintVal = "1"
+                delete = GetClass()
+                delete.deleteData(table,colConstraint,colConstraintVal)
+            
 
 
 
